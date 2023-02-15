5d3d9f3528b3706014be24f13dd9a23f10d9a24f
AnderVaruskin <115535231+AnderVaruskin@users.noreply.github.com>
2023-02-15 11:18:04 +0200

    Add files via upload

---
+++ Kilpkonn.py
@@ -0,0 +1,45 @@
+import turtle 
+
+
+def read_instructions(filename): 
+    with open(filename, "r") as file: 
+        instructions = file.readlines()
+        return instructions
+
+taust = turtle.Screen()
+taust.title("Kilpkonnajoonis - Varuskin")
+taust.bgcolor("red")
+
+def parse_instructions(instructions):
+    for i in range(len(instructions)):
+        instructions[i] = instructions[i].strip().split()
+        instructions[i][1] = int(instructions[i][1])
+    return instructions
+
+def draw_shape(instructions): 
+    t = turtle.Turtle() 
+    for instruction in instructions:
+        command, value = instruction
+        if command == "edasi": 
+            t.forward(value)
+        elif command == "tagasi": 
+            t.backward(value)
+        elif command == "paremale": 
+            t.right(value)
+        elif command == "vasakule": 
+            t.left(value)
+    
+    
+    
+    turtle.done()
+
+
+
+def main():
+    filename = "kilpkonn.txt"
+    instructions = read_instructions(filename)
+    parsed_instructions = parse_instructions(instructions)
+    draw_shape(parsed_instructions)
+
+if __name__ == "__main__":
+    main()
